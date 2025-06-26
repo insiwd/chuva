69eb4dd2761d8683e1b046ab165b00f54f266b7a
ruan <ruan.cavcalcante505@gmail.com>
2025-06-26 14:20:02 -0300

    feat: campo de busca

--- js/main.js
+++ js/main.js
@@ -1,7 +1,28 @@
-const cityName = document.getElementById('cityName');
+const cityName = document.getElementById("cityName");
+const cityInput = document.getElementById("cityInput");
 
-cityName.addEventListener('click', changeCity)
+cityName.addEventListener("click", () => {
+    cityName.style.display = "none";
+    cityInput.style.display = "inline";
 
-function changeCity() {
-  
-}
+    cityInput.value = cityName.textContent.trim();
+    cityInput.focus();
+});
+
+//ao perder o foco
+cityInput.addEventListener("blur", () => {
+    const newCity = cityInput.value.trim();
+    if (newCity) {
+        cityName.textContent = newCity;
+        // TODO: chamar API
+    }
+    cityInput.style.display = "none";
+    cityName.style.display = "inline";  
+})
+
+// enter
+cityInput.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        cityInput.blur();
+    }
+})
